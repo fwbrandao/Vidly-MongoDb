61e08e1a0356f0f07ed935aa76c3b84a85f835f3
Fernando Brandao <fwbrandao@gmail.com>
2019-02-23 13:47:28 +0000

    test

--- routes/genres.js
+++ routes/genres.js
@@ -36,7 +36,7 @@ router.put("/:id", [auth, validateObjectId], async (req, res) => {
   );
 
   if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
+    return res.status(404).send("1 The genre with the given ID was not found.");
 
   res.send(genre);
 });
@@ -45,7 +45,7 @@ router.delete("/:id", [auth, admin, validateObjectId], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
+    return res.status(404).send("2 The genre with the given ID was not found.");
 
   res.send(genre);
 });
@@ -54,7 +54,7 @@ router.get("/:id", validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id).select("-__v");
 
   if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
+    return res.status(404).send("3 The genre with the given ID was not found.");
 
   res.send(genre);
 });
